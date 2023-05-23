7ec4390b29e4e9888c0bc3d20887be2d6e647370
ChrisPos <chrispos666@email.com>
2023-05-23 16:57:52 +0200

    tak

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
-const express = require('express');
-const axios = require('axios');1
+const express = require('express'); // to jest zeby postawic serwer http
+const axios = require('axios'); // to jest zeby polaczyc sie do zewnetrzego api w naszym przypadku CEPIK
+const sqlite3 = require('sqlite3'); // to jest do bazy danych w naszym przypadku SQLite
 
 
+// Tworzenie instancji aplikacji Express
 const app = express();
 const port = 3000;
+
+// Tworzenie obiektu bazy danych SQLite
+const db = new sqlite3.Database(':memory:'); // Tworzy bazę danych w pamięci, można również użyć ścieżki do pliku na dysku
+
+
+
+// Tworzenie tabeli Users
+db.serialize(() => {
+  db.run('CREATE TABLE Users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)');
+});
+
+
+// Endpoint GET, pobierający wszystkie rekordy z tabeli Users
+app.get('/users', (req, res) => {
+  db.all('SELECT * FROM Users', (err, rows) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(rows);
+    }
+  });
+});
+
+
+db.connect((err) => {
+  if (err) {
+    console.error('Błąd połączenia z bazą danych:', err.message);
+  } else {
+    console.log('Połączono z bazą danych SQLite');
+
+    // Tutaj możesz dodać dodatkową logikę lub uruchomić serwer Express
+    app.listen(3000, () => {
+      console.log('Serwer Express nasłuchuje na porcie ${port}');
+    });
+  }
+});
+
+
 
 // Domyślne powitanie
 app.get('/', async (req, res) => {
@@ -13,9 +53,17 @@ app.get('/', async (req, res) => {
     res.send(data);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Wystąpił błąd podczas komunikacji z API.');
+    res.status(500).send('Wystąpił błąd podczas komunikacji z CEPIK API.');
   }
 });
+
+
+
+
+
+
+
+
 
 // Endpoint do odczytu wszystkich danych
 app.get('/data', (req, res) => {
@@ -39,9 +87,9 @@ app.post('/data', (req, res) => {
   res.json(newData);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
+
+
 
 //TRZEBA URUCHOMIĆ KOMENDĘ npm install express
 //POTEM http://localhost:3000/
